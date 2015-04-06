e2fa1bdb13bd66a93d7c2aafe6f98ef4aa5b47d3
KKMAN <riceholicman@163.com>
2015-04-06 22:04:13 +0800

    create Yamba

---
+++ Yamba/src/com/example/yamba/StatusActivity.java
@@ -0,0 +1,86 @@
+package com.example.yamba;
+
+import winterwell.jtwitter.Twitter;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class StatusActivity extends Activity {
+	
+	private static final String TAG="StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+	TextView textCount;
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
+		
+		textCount = (TextView) findViewById(R.id.textView2);
+		editText = (EditText) findViewById(R.id.editText1);
+		updateButton = (Button) findViewById(R.id.button1);
+		updateButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				twitter.setStatus(editText.getText().toString());
+				Log.d(TAG, "onClicked");
+			}
+		}
+		);
+		
+		editText.addTextChangedListener(new TextWatcher(){
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				int count = 140 - s.length();
+				textCount.setText(count);
+				textCount.setTextColor(Color.GREEN);
+				if(count < 10)
+					textCount.setTextColor(Color.YELLOW);
+				if(count < 0)
+					textCount.setTextColor(Color.RED);
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+				
+			}
+		}
+		);
+		
+		twitter = new Twitter("student","password");
+		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.status, menu);
+		return true;
+	}
+
+}
